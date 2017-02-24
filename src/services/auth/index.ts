4140f3ce721916f1ce398285ed476edb515bf6c4
Jan Horubała <jan.horubala@gmail.com>
2017-02-24 11:21:39 +0100

    Restructure project

---
+++ src/services/auth/index.ts
@@ -0,0 +1,4 @@
+export {default as login} from './login'
+export {default as logout} from './logout'
+export {default as signin} from './signin'
+export {default as verify} from './verify'
---
+++ src/services/auth/logout.ts
@@ -0,0 +1,4 @@
+
+export default function logout(res, req, next) {
+
+}
