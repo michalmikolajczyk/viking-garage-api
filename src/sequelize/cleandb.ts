3f996435a6e55f07435bb50803c49da83300751f
Jan Horubała <jan.horubala@gmail.com>
2017-02-26 20:54:59 +0100

    Implemented tests for user/login endpoint

--- src/sequelize/cleandb.ts
+++ src/sequelize/cleandb.ts
@@ -2,20 +2,18 @@ import { User } from './'
 
 export default function cleandb() {
   // recreate User table
-  return User.sync({ force: true }).then(function() {
-    // create new User
-    return User.create({
-      name: 'Viking Garage',
-      email: 'viking@garage.com',
-      birthday: new Date(1980, 12, 12),
-      password: 'secret',
-      verified: true
+  return User.sync({ force: true })
+    .then(function() {
+      // create new User
+      return User.create({
+        name: 'Viking Garage',
+        email: 'viking.garage.app@gmail.com',
+        birthday: new Date(1980, 12, 12),
+        password: 'secret',
+        verified: true,
+      })
+      .catch(err => {
+        console.log('Database clean error', err)
+      })
     })
-    .then(user => {
-      console.log('Database cleaned, user created', user.dataValues)
-    })
-    .catch(err => {
-      console.log('Database clean error', err)
-    })
-  })
 }
