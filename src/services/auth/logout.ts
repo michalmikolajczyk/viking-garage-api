f0b1f99d7103200f18207510a5012b617226e06a
Jan Horubała <jan.horubala@gmail.com>
2017-02-28 13:51:22 +0100

    Implemented JWT auth in other endpoints

--- src/services/auth/logout.ts
+++ src/services/auth/logout.ts
@@ -1,5 +1,5 @@
 export default function logout(res, req, next) {
-  req.logout()
+  // because of JWT, there is no session - you have to destroy token on client side
   res.json({
     err: false,
     msg: 'User log out successfully'
