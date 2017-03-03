d455dc422a240b4567c54604e0c480273946efb1
Jan Horubała <jan.horubala@gmail.com>
2017-03-03 10:53:13 +0100

    Fixed styles in src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,37 @@
-import './controllers'
-import * as express from 'express'
-import * as cors from 'cors'
-import * as bodyParser from 'body-parser'
-import * as methodOverride from 'method-override'
-import { RegisterRoutes } from './routes'
-import { config as passportConfig } from './helpers/passport'
-import config from './config'
+import './controllers';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import * as methodOverride from 'method-override';
+import { RegisterRoutes } from './routes';
+import { config as passportConfig } from './helpers/passport';
+import config from './config';
 
-const app = express()
+const app = express();
 
 app.use('/docs', express.static(__dirname + '/swagger-ui'));
 app.use('/swagger.json', (req, res) => {
-  res.sendfile('./dist/swagger.json')
-})
+  res.sendfile('./dist/swagger.json');
+});
 
 app.use(cors({
-  origin: config['origin'],
+  origin: config.origin,
   credentials: true,
-}))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(methodOverride());
 
-passportConfig(app)
-RegisterRoutes(app)
+passportConfig(app);
+RegisterRoutes(app);
 
-app.use(function(err, req, res, next) {
-  console.log(`Unexpected error ${err}`)
-  res.status(500).send(`Unexpected error ${err}`)
-})
+app.use((err, req, res, next) => {
+  console.log(`Unexpected error ${err}`);
+  res.status(500).send(`Unexpected error ${err}`);
+});
 
-app.listen(config['port'], function() {
-  console.log(`Server running at ${config['host']}:${config['port']}`)
-})
+app.listen(config.port, () => {
+  console.log(`Server running at ${config.host}:${config.port}`);
+});
 
-export default app
+export default app;
