9da855fa05417a3f953ba950ced7c2c8af454c68
Michal Mikolajczyk <michal@trewebs.com>
2017-05-18 00:25:10 +0200

    corrected linting, test dependencies fail?

--- src/services/offer/getAll.ts
+++ src/services/offer/getAll.ts
@@ -10,16 +10,16 @@ const query = (point, dist) =>
   `SELECT * FROM offers WHERE ST_DWithin(coord, ST_SetSRID(ST_Point(${point[0]}, ${point[1]}), 4326), ${dist});`;
 
 export default function getAll(req: Request, res: Response, next: NextFunction): any {
-  let lat = req.query.lat || 52.237684;
-  let lng = req.query.lng || 21.030086;
-  let dist = req.query.dist || 10000;
+  const lat = req.query.lat || 52.237684;
+  const lng = req.query.lng || 21.030086;
+  const dist = req.query.dist || 10000;
 
   db['sequelize'].query(query([lat, lng], dist), { type: db['sequelize'].QueryTypes.SELECT })
     .then((offers) => {
       if (!offers) return res.status(400).json(err.notExists);
       res.json(offers);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(`Unexpected error ${err}`);
       res.status(500).json(err.unexpected);
     });
