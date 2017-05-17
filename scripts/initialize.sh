ca03e4169090dab91c77aae1cf02eda0e643f447
Michal Mikolajczyk <michal@trewebs.com>
2017-05-18 00:18:22 +0200

    mid-work-WIP

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-brew update
-brew install postgres
-brew install postgis
+# brew update
+# brew install postgres
+# brew install postgis
 
-postgres -D /usr/local/var/postgres
+postgres -D /usr/local/var/postgres >logfile 2>&1 &
 
-createuser -P viking  # provide username & password
+createuser -w viking  # provide username & password
 createdb test_db      # create database for prod, test and dev
 psql test_db -c "GRANT ALL PRIVILEGES ON DATABASE test_db TO viking"
 psql test_db -c "CREATE EXTENSION postgis"
 
-(cd ../src && cp config.ts.example config.ts)
+(cd src && cp config.ts.example config.ts)
 
 npm i
 npm test
 npm run dev
+
