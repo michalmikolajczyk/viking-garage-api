418951c7f797406ec60f5f58786a16a7f9a9d49c
Michal Mikolajczyk <michal@trewebs.com>
2017-05-18 16:32:32 +0200

    heroku compliance, API

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -1,12 +1,19 @@
 import Sequelize from 'sequelize';
 import config from '../config';
 
-const db = config.database;
-const sequelize = new Sequelize(db.name, db.username, db.password, {
-  host: db.host || 'localhost',
-  port: db.port || 5432,
-  dialect: 'postgres',
-  logging: false,
-});
+var db = process.env.DATABASE_URL || config.database;
+// var sequelize = "";
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(db)
+} else {
+    sequelize = new Sequelize(db.name, db.username, db.password, {
+      host: db.host || 'localhost',
+      port: db.port || 5432,
+      dialect: 'postgres',
+      logging: false,
+    });
+}
 
 export default sequelize;
