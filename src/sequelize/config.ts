89bff5064bf76bacc091d95368e70391d894559b
Jan Horubała <jan.horubala@gmail.com>
2017-05-24 17:18:48 +0200

    Added to sequelize config db from env var

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -1,10 +1,21 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  dialect: 'postgres',
-  logging: false,
-});
+let sequelize;
+
+if (typeof process.env.DATABASE_URL !== 'undefined') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+  });
+}
 
 export default sequelize;
