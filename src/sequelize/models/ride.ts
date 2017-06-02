e2745cd547974be5664699d30f990c0b9e0ded7e
Jan Horubała <jan.horubala@gmail.com>
2017-06-02 19:50:36 +0200

    Added price, total & currency field to booking + validation

--- src/sequelize/models/ride.ts
+++ src/sequelize/models/ride.ts
@@ -7,26 +7,42 @@ export default function (sequelize, Sequelize) {
     email: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: { isEmail: true },
     },
     offer: {
       type: Sequelize.INTEGER,
       allowNull: false,
-    },
-    total: {
-      type: Sequelize.DECIMAL(19, 4),
-      allowNull: false,
+      validate: { isInt: true },
     },
     equipment: {
       type: Sequelize.INTEGER,
       allowNull: false,
+      validate: { isInt: true },
     },
     startDate: {
       type: Sequelize.DATE,
       allowNull: false,
+      validate: { isDate: true },
     },
     endDate: {
       type: Sequelize.DATE,
       allowNull: false,
+      validate: { isDate: true },
+    },
+    price: {
+      type: Sequelize.DECIMAL(19, 4),
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+    total: {
+      type: Sequelize.DECIMAL(19, 4),
+      allowNull: false,
+      validate: { isDecimal: true },
+    },
+    currency: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: { isAlpha: true },
     },
   });
 }
