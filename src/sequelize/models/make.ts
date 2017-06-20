afea4ab00b97cc59e11538850cf88efe66244b6c
Jan Horubała <jan.horubala@gmail.com>
2017-06-20 21:18:39 +0200

    Removed unused models

--- src/sequelize/models/make.ts
+++
@@ -1,9 +0,0 @@
-export default function (sequelize, Sequelize) {
-  return sequelize.define('make', {
-    name: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.STRING,
-    },
-  });
-};
