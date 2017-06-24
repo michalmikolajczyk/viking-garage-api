092ba8d27ff7c7e3ddc16bf23a2c89e991a4aa96
Jan Horubała <jan.horubala@gmail.com>
2017-06-24 18:21:55 +0200

    Turn on loggin in sequelize

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -14,7 +14,6 @@ if (typeof process.env.DATABASE_URL !== 'undefined') {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    logging: false,
   });
 }
 
