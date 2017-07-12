b411adb7f23aa45ded2e7518f0ce49a9c5515fa6
Jan Horubała <jan.horubala@gmail.com>
2017-07-12 23:27:49 +0200

    Updated sequlize & passport to new env vars

--- src/sequelize/config.ts
+++ src/sequelize/config.ts
@@ -1,20 +1,10 @@
 import Sequelize from 'sequelize';
 
-let sequelize;
-
-if (typeof process.env.DATABASE_URL !== 'undefined') {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: true,
-    },
-  });
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-  });
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.DATABASE_SSL == 'true',
+  },
+});
 
 export default sequelize;
